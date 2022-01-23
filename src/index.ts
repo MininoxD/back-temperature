742e0b2b98585e7acf6a28ac78417c935de6feb2
Luis Minino <luis@minino.miau>
2022-01-23 17:01:42 -0500

    Update cors multple

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,8 @@ const app = express()
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header('Access-Control-Allow-Credentials', "true");
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    res.header("Access-Control-Allow-Origin", "https://frontend-temperature.vercel.app"); 
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
