a61e2d81816a8fc80be3f00147f744da74d9cde9
Luis Minino <luis@minino.miau>
2022-01-23 16:27:50 -0500

    update 4

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 /* server socket io */
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://frontend-temperature.vercel.app/"],
     methods: ["GET", "POST"]
 }});
 
