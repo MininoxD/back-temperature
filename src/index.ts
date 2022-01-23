efe5f0381e6b35d4601f8407e9184d6511fc7ce7
Luis Minino <luis@minino.miau>
2022-01-23 16:50:41 -0500

    Cors express

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,16 @@ const port:number = 5000
 /* server express */
 const app = express()
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 /* server socket io */
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: {
